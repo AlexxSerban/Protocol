1746e372a4ec4d3b37862d8af0ccbb20c741fda0
AlexxSerban <109248856+AlexxSerban@users.noreply.github.com>
2023-09-12 19:17:18 +0300

    .

---
+++ Protocol/Protocol/ProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolApp.swift
+//  Protocol
+//
+//  Created by Alex Serban on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
